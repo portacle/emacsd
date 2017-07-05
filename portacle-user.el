df042b1aad99d5fffaceb430ff1e9303e1687930
Shinmera <shinmera@tymoon.eu>
2017-07-05 17:13:16 +0200

    Don't prompt about customize.

--- portacle-user.el
+++ portacle-user.el
@@ -1,9 +1,5 @@
 (provide 'portacle-user)
 
-;; Make sure to prompt to save changes in customize
-(add-hook 'kill-emacs-query-functions
-          'custom-prompt-customize-unsaved-options)
-
 (setq custom-file (portacle-path "config/user.el"))
 (when (file-exists-p (portacle-path "config/user.el"))
   (load (portacle-path "config/user.el")))
