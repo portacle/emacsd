fec66f985b21b109613538dccb98428c96cc8bd5
João Távora <joaotavora@gmail.com>
2018-07-11 16:13:32 +0100

    emacs-slime-nav is kinda useless in emacs 25 which has xref
    * portacle-elisp.el (emacs-slime-nav): Don't require this

--- portacle-elisp.el
+++ portacle-elisp.el
@@ -1,6 +1,3 @@
 (provide 'portacle-elisp)
 
-(ensure-installed 'elisp-slime-nav 'paredit)
-
-(require 'elisp-slime-nav)
-(add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode)
+(ensure-installed 'paredit)
